c22b79c6a311018af6f44de205cabc69d3e77ff7
Andrei Hutanu <andrei.hutanu@evalan.com>
2020-06-28 14:32:38 +0200

    added symlink and uninstall script

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,11 +1,21 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+. "$toolsDir\commonEnv.ps1"
+
 $url        = 'ftp://ftp.fu-berlin.de/pc/games/idgames/levels/doom2/Ports/megawads/nova3.zip'
+$unzipLocation = "$(Join-Path $toolsDir wad)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  unzipLocation = "$(Join-Path $toolsDir wad)"
+  unzipLocation = $unzipLocation
   url           = $url
   checksum      = '234DF07D3C13FB97EC43A3ABF129114B2169AF597EF1DB02521CB4DD9BF0814B'
   checksumType  = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
+
+# Cleanup link first (safely)
+& $ENV:COMSPEC /C IF EXIST $wad_lnk DEL /S /Q $wad_lnk
+
+# Install symbolic link for later use
+$wad_src = "$(Join-Path $unzipLocation $wad)"
+& $ENV:COMSPEC /C MKLINK $wad_lnk $wad_src
---
+++ tools/commonEnv.ps1
@@ -0,0 +1,2 @@
+$wad = 'NOVA3.wad'
+$wad_lnk = "$(Join-Path $ENV:DOOMWADDIR $wad)"
